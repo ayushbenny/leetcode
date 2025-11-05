5412b9f001b30c60ff2f01b320b451d2509e213c
AMBUSH4R <38602506+ayushbenny@users.noreply.github.com>
2025-11-05 10:17:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (12.88%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        same_position = 0
+        first_pos = 1
+        second_pos = 2
+        a = x-z
+        b = y-z
+        print(abs(a),abs(b))
+        if abs(a) == abs(b):
+            return same_position
+        elif abs(a) < abs(b):
+            return first_pos
+        else:
+            return second_pos
